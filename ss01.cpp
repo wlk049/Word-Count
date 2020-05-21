4bc2e78de1492ccc16036c797fd480355b3a7eb7
wlk049 <937966176@qq.com>
2020-05-21 10:16:37 +0800

    123

---
+++ ss01.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+main(int argc,char** argv)
+{
+    int sum=0;
+    char tmp;
+	char x[]={'-','w'};
+	printf("%d\n",argc);
+    printf("%s\n",argv[0]);
+    printf("%s\n",argv[1]);
+    printf("%s\n",argv[2]);
+    char str[20];
+
+
+    FILE *fp=fopen(argv[2],"r");
+    if(strcmp(argv[1],x)==0)
+    {
+    if(fp==NULL)
+    {
+        return 0;
+    }
+    while(!feof(fp))
+    {
+        fgetc(fp);
+        sum++;
+    }
+    fclose(fp);
+    printf("出现的字符数 :  %d\n",sum);
+    }
+    else
+    {        
+    if(fp==NULL)
+    {
+        return 0;
+    }
+    while(!feof(fp))
+    {
+        tmp = fgetc(fp);
+        if(tmp == ' '||tmp==',')
+		sum++;
+    }
+    fclose(fp);
+    sum++; 
+    printf("所出现的单词数 :  %d\n",sum);
+    }
+    return 0;
+}
